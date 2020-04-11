1ee76f0fa52dc784b44149f537b4b2a9c49a4167
REXJJ <rexjj7@gmail.com>
2020-04-11 03:31:36 +0000

    Created Workspace

---
+++ Project2/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/REX_WS/Udacity-Robotics-Engineer/Project2/catkin_ws/src
+# Build directory: /home/workspace/REX_WS/Udacity-Robotics-Engineer/Project2/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ Project2/catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/workspace/REX_WS/Udacity-Robotics-Engineer/Project2/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
